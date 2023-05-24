2cee355ce4247f72ff73c00ebf21498aa8eb6c23
Christoph Osswald <christoph.osswald@dvzo.ch>
2023-05-24 16:12:24 +0200

    Add activities to training
    Also add some icons to apps

--- addons/training/models/training.py
+++ addons/training/models/training.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class Training(models.Model):
     _name = "training.training"
     _description = "Trainings"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char("Short Name", required=True)
     label = fields.Char("Label", required=True)
@@ -14,6 +15,28 @@ class Training(models.Model):
     meeting_place = fields.Char("Meeting place")
     take_along = fields.Html("Take along")
     important = fields.Html("Important")
+    type = fields.Selection(
+        selection=[
+            ("training", "Training course"),
+            ("further_training", "Further training"),
+        ],
+        string="Type",
+        default="training",
+        required=True,
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("planned", "Planned"),
+            ("running", "Running"),
+            ("archived", "Archived"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="planned",
+    )
 
     participant_ids = fields.One2many("training.participant", "training", "Participants")
     training_date_ids = fields.One2many("training.date", "training", "Dates")
