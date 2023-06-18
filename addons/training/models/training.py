3f20946f66f3f83fffb72444960181f3a38e7496
Christoph Osswald <christoph.osswald@dvzo.ch>
2023-06-18 10:24:20 +0200

    Add actions to training for setting state

--- addons/training/models/training.py
+++ addons/training/models/training.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 
 class Training(models.Model):
@@ -9,7 +10,7 @@ class Training(models.Model):
     name = fields.Char("Short Name", required=True)
     label = fields.Char("Label", required=True)
     module = fields.Many2one("training.module")
-    responsible = fields.Many2one("res.partner", string="Responsible",  copy=False, required=True)
+    responsible = fields.Many2one("res.partner", string="Responsible", copy=False, required=True)
     start = fields.Date("Start date")
     end = fields.Date("End date")
     meeting_place = fields.Char("Meeting place")
@@ -40,3 +41,31 @@ class Training(models.Model):
 
     participant_ids = fields.One2many("training.participant", "training", "Participants")
     training_date_ids = fields.One2many("training.date", "training", "Dates")
+
+    def action_running(self):
+        if "archived" in self.mapped("state"):
+            raise UserError("Archived trainings can't have state 'running'.")
+        elif "canceled" in self.mapped("state"):
+            raise UserError("Canceled trainings can't have state 'running'.")
+        return self.write({"state": "running"})
+
+    def action_archived(self):
+        if "canceled" in self.mapped("state"):
+            raise UserError("Canceled trainings can't have state 'archived'.")
+        self.write({"state": "archived"})
+        self.action_archive()
+        return True
+
+    def action_canceled(self):
+        if "archived" in self.mapped("state"):
+            raise UserError("Archived trainings can't have state 'canceled'.")
+        self.write({"state": "canceled"})
+        self.action_archive()
+        return True
+
+    def action_planned(self):
+        if "archived" in self.mapped("state"):
+            raise UserError("Archived trainings can't have state 'planned'.")
+        elif "canceled" in self.mapped("state"):
+            raise UserError("Canceled trainings can't have state 'planned'.")
+        return self.write({"state": "planned"})
