2de97862b5bd92dc51cccb56f013abed7586d56a
Christoph Osswald <christoph.osswald@dvzo.ch>
2023-10-07 11:49:10 +0200

    Add default displays to key_lending and key

--- addons/key_management/models/key.py
+++ addons/key_management/models/key.py
@@ -1,9 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Key(models.Model):
     _name = "key_management.key"
     _description = "Key"
+    _rec_name = "computed_name"
 
     name = fields.Char("Number", required=True)
     key_group = fields.Many2one("key_management.group", required=True)
+    computed_name = fields.Char(compute="_compute_name", store=True)
+
+    @api.depends('name', 'key_group.name', 'key_group.key_system.name')
+    def _compute_name(self):
+        for record in self:
+            if record.name and record.key_group.name:
+                record.computed_name = f"{record.key_group.key_system.name} - {record.key_group.name} - {record.name}"
+            else:
+                record.computed_name = record.name
--- addons/key_management/models/key_lending.py
+++ addons/key_management/models/key_lending.py
@@ -1,12 +1,21 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class KeyLending(models.Model):
     _name = "key_management.lending"
     _description = "Key lending"
+    _rec_name = "computed_name"
 
     lender = fields.Many2one("res.partner", string="Lender",  copy=False, required=True)
     key = fields.Many2one("key_management.key", required=True)
     lending_date = fields.Date(copy=False, default=fields.Date.today())
     return_date = fields.Date(copy=False)
+    computed_name = fields.Char(compute="_compute_name", store=True)
 
+    @api.depends('lender', 'key')
+    def _compute_name(self):
+        for record in self:
+            if record.lender and record.key:
+                record.computed_name = f"{record.key.name} - {record.lender.name}"
+            else:
+                record.computed_name = record.lender.name
