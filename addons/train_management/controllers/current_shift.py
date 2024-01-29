4a5a029b5216c67ee49a55e0b17960c9fd1bf872
Christoph Osswald <christoph.osswald@dvzo.ch>
2024-01-29 15:32:10 +0100

    add current shift view to portal

--- addons/train_management/controllers/current_shift.py
+++ addons/train_management/controllers/current_shift.py
@@ -1,10 +1,24 @@
 from odoo.http import request
 from odoo.http import Controller
 from odoo.http import route
+from datetime import date
 
 
 class CurrentShiftController(Controller):
 
     @route("/my/shift/current/", website=True, auth="user")
     def list_view(self):
-        return request.render("train_management.web_current_shifts_list_view")
+        past_dayplanning_shifts = request.env['train_management.day_planning_shift'].sudo().search([
+            ("person", "=", request.env.user.partner_id.id),
+            ("day_planning_date", ">=", date.today())])
+        current_shifts = []
+        for day_planning_shift in past_dayplanning_shifts:
+            shift = day_planning_shift.shift
+            current_shifts.append({
+                "day_planning": day_planning_shift.day_planning,
+                "shift": shift
+                })
+        current_shifts = sorted(current_shifts, key=lambda shift: shift['day_planning'].date)
+        return request.render("train_management.web_current_shifts_list_view", {
+            'current_shifts': current_shifts,
+        })
